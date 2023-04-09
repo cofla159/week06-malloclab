24ad99616aaddc8cb454e8bf667fe054bcd1b6b6
황채림 <cofla159@naver.com>
2023-04-09 16:59:08 +0000

    address_in_order 방식 sudo code

--- mm.c
+++ mm.c
@@ -50,31 +50,63 @@ team_t team = {
 #define GET_SIZE(p) (GET(p) & ~0x7)
 #define GET_ALLOC(p) (GET(p) & 0x1)
 
-#define HDRP(p) ((char *)(p)-WSIZE)                            // 헤더 주소
-#define FTRP(p) ((char *)(p) + GET_SIZE(HDRP(p)) - DSIZE)      // 푸터 주소
-#define NEXT_BLKP(p) ((char *)(p) + GET_SIZE(HDRP(p)))         // (물리적) 다음 블록 주소
-#define PREV_BLKP(p) ((char *)(p)-GET_SIZE((char *)(p)-DSIZE)) // (물리적) 이전 블록 주소
-#define GET_NEXT_HDR(p) (HDRP(NEXT_BLKP(p)))                   // 다음 블록 헤더값 가져오기
-#define GET_PREV_HDR(p) (HDRP(PREV_BLKP(p)))                   // 다음 블록 헤더값 가져오기
-#define GET_NEXT_FTR(p) (FTRP(NEXT_BLKP(p)))                   // 다음 블록 헤더값 가져오기
-#define GET_PREV_FTR(p) (FTRP(PREV_BLKP(p)))                   // 다음 블록 헤더값 가져오기
-#define PRED_PRED(p) (*GET(p))                                 // predecessor의 predecessor가 가지고 있는 값
-#define SUCC_SUCC(p) (*GET(p + WSIZE))                         // successor의 successor가 가지고 있는 값
+#define HDRP(bp) ((char *)(bp)-WSIZE)                             // 헤더 주소
+#define FTRP(bp) ((char *)(bp) + GET_SIZE(HDRP(bp)) - DSIZE)      // 푸터 주소
+#define NEXT_BLKP(bp) ((char *)(bp) + GET_SIZE(HDRP(bp)))         // (물리적) 다음 블록 주소
+#define PREV_BLKP(bp) ((char *)(bp)-GET_SIZE((char *)(bp)-DSIZE)) // (물리적) 이전 블록 주소
+#define GET_NEXT_HDR(bp) (HDRP(NEXT_BLKP(bp)))                    // 다음 블록 헤더값 가져오기
+#define GET_PREV_HDR(bp) (HDRP(PREV_BLKP(bp)))                    // 다음 블록 헤더값 가져오기
+#define GET_NEXT_FTR(bp) (FTRP(NEXT_BLKP(bp)))                    // 다음 블록 헤더값 가져오기
+#define GET_PREV_FTR(bp) (FTRP(PREV_BLKP(bp)))                    // 다음 블록 헤더값 가져오기
+#define PRED(bp) (bp)                                             // predecessor의 위치는 bp와 일치함
+#define SUCC(bp) (bp + WSIZE)
+#define PRED_PRED(bp) (*GET(bp))         // predecessor의 predecessor가 가지고 있는 값
+#define SUCC_SUCC(bp) (*GET(bp + WSIZE)) // successor의 successor가 가지고 있는 값
+
+// allocate할 때는 사용하는 블록은 링크드리스트에서 빼버리고 pred의 succ->succ 가리키고 succ->pred->pred 가리켜야 함
+// free할 때는 coalesce가 알아서 해줄거임
+
+static void *find_linked_prev(void *ptr)
+{
+    // ptr의 prev에서 시작.
+    // prev가 free면 그 주소 리턴.
+    // alloc이면 하나 더 prev로 업데이트.
+    // mem_heap_lo까지 갔는데 없으면 root 리턴.
+}
+
+static void *find_linked_next(void *ptr)
+{
+    // ptr의 next에서 시작.
+    // next가 free면 그 주소 리턴.
+    // alloc이면 하나 더 next로 업데이트.
+    // mem_heap_hi까지 갔는데 없으면 null 리턴.
+}
 
 static void *coalesce(void *ptr)
 {
+    // 합치고 난 뒤에 predecessor랑 successor업데이트 해줘야 함.
+    // coalesce는 물리적으로 앞뒤로 합치는거고 링크드리스트도 앞뒤로 합침 필요.
+    // 넣을 위치 찾을 함수 필요. ptr의 앞쪽에 있는 free block이랑 뒤쪽에 있는 free block 찾기.
+    // = list_prev, list_next
+
     size_t prev_allocated = GET_ALLOC(GET_PREV_FTR(ptr));
     size_t next_allocated = GET_ALLOC(GET_NEXT_HDR(ptr));
     size_t now_size = GET_SIZE(HDRP(ptr));
-    // 합치고 난 뒤에 predecessor랑 successor 업데이트 해줘야 함.
-    if (prev_allocated && !next_allocated)
+
+    if (prev_allocated && next_allocated)
     {
+        // 합칠게 없으면 SUCC(list_prev) = ptr, PRED(ptr) = list_prev , PREV(next) = ptr, SUCC(ptr) = list_next
+    }
+    else if (prev_allocated && !next_allocated)
+    {
+        // 다음거랑 합치면 SUCC(list_prev) = ptr, PRED(ptr) = list_prev, SUCC(ptr) = SUCC(list_next), PRED(SUCC(list_next)) = ptr
         now_size += GET_SIZE(GET_NEXT_HDR(ptr));
         PUT(HDRP(ptr), PACK(now_size, 0));
         PUT(FTRP(ptr), PACK(now_size, 0));
     }
     else if (!prev_allocated && next_allocated)
     {
+        // 이전꺼랑 합치면 링크드리스트는 업데이트 없음.
         now_size += GET_SIZE(GET_PREV_HDR(ptr));
         PUT(FTRP(ptr), PACK(now_size, 0));
         PUT(GET_PREV_HDR(ptr), PACK(now_size, 0));
@@ -82,6 +114,7 @@ static void *coalesce(void *ptr)
     }
     else if (!prev_allocated && !next_allocated)
     {
+        // 양쪽 다 합치면 SUCC(list_prev) = SUCC(list_next), PREV(SUCC(list_next)) = list_prev
         now_size += GET_SIZE(GET_PREV_FTR(ptr)) + GET_SIZE(GET_NEXT_HDR(ptr));
         PUT(GET_PREV_HDR(ptr), PACK(now_size, 0));
         PUT(GET_NEXT_FTR(ptr), PACK(now_size, 0));
@@ -93,14 +126,17 @@ static void *coalesce(void *ptr)
 static void *extend_heap(size_t words)
 {
     size_t size = words % 2 ? (words + 1) * WSIZE : words * WSIZE;
-    void *start_p = mem_sbrk(size);
+    char *start_p = mem_sbrk(size);
     if (start_p == (void *)-1)
     {
         return NULL;
     }
+    // 새롭게 할당되는 블록도 free니까 predecessor랑 successor가 있음
+    // succ = null이고 pred 링크드리스트 마지막 블록
+    // coalesce가 다 해줄거임
     PUT(HDRP(start_p), PACK(size, 0));
     PUT(FTRP(start_p), PACK(size, 0));
-    PUT(GET_NEXT_HDR(start_p), PACK(0, 1));
+    PUT(GET_NEXT_HDR(start_p), PACK(0, 1)); // 마지막 워드에 에필로그 넣어주기
 
     return coalesce(start_p);
 }
@@ -115,11 +151,12 @@ int mm_init(void)
     {
         return -1;
     }
-    PUT(heap_startp, 0);                          // 패딩
-    PUT(heap_startp + WSIZE, PACK(DSIZE, 1));     // 프롤로그 헤더
-    PUT(heap_startp + WSIZE * 2, PACK(DSIZE, 1)); // 프롤로그 푸터
-    // 여기다가 initial predecessor랑 successor가 추가되어야 함.
-    PUT(heap_startp + WSIZE * 3, PACK(0, 1)); // 에필로그
+    PUT(heap_startp, 0);                              // 패딩
+    PUT(heap_startp + WSIZE, PACK(2 * DSIZE, 1));     // 프롤로그 헤더
+    PUT(heap_startp + WSIZE * 2, NULL);               // 힙의 첫번째 predecessor = root 역할
+    PUT(heap_startp + WSIZE * 3, NULL);               // successor
+    PUT(heap_startp + WSIZE * 4, PACK(2 * DSIZE, 1)); // 프롤로그 푸터
+    PUT(heap_startp + WSIZE * 5, PACK(0, 1));         // 에필로그
 
     if (extend_heap(CHUNKSIZE / WSIZE) == NULL)
     {
@@ -130,25 +167,11 @@ int mm_init(void)
 
 static void *find_fit(size_t asize)
 {
-    char *start_p = mem_heap_lo() + DSIZE; // root(init할 때의 successor)
+    char *start_p; // root가 가리키는 곳에 가서 할당여부&크기 확인.
 
-    // root가 가리키는 곳에 가서 할당여부&크기 확인.
     // 할당할 수 있다면 predecessor와 successor 포인터를 업데이트 해준 뒤 포인터 리턴.
     // 할당할 수 없으면 현재 포인터를 *successor로 업데이트 해서 윗줄로 돌아가기.
     // 링크드리스트 끝(처음 init할 때의 initial block)까지 다 봤는데도 없으면 return NULL.
-
-    // while (1)
-    // {
-    //     if (find_p > (char *)mem_heap_hi())
-    //     {
-    //         return NULL;
-    //     }
-    //     if (!GET_ALLOC(HDRP(find_p)) && GET_SIZE(HDRP(find_p)) >= asize)
-    //     {
-    //         return (void *)find_p;
-    //     }
-    //     find_p = NEXT_BLKP(find_p);
-    // }
 
     // while (1)
     // {
