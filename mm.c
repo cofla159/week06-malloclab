6896e9b20900eb226aa881e8a387736215b06647
황채림 <cofla159@naver.com>
2023-04-10 09:42:11 +0000

    explicit-디버깅 중간 저장 1. short1 테스트 케이스에서 첫번째 해제 후 프리블록 coalesce 시 첫번째 if문으로 들어가야 하는데 next_allocated인 에필로그의 값이 0이 아님 2. place 함수의 if문에서 원래 블럭이 링크드리스트의 마지막 원소였을 때, SUCC(bp)가 0이어야 하는데 에러가 나지 않는 것으로 보아 아닌 것 같음

--- mm.c
+++ mm.c
@@ -38,14 +38,17 @@ team_t team = {
 #define WSIZE 4
 #define DSIZE 8
 #define CHUNKSIZE (1 << 12)
+#define __POINTER_TYPE__ unsigned int
+typedef __POINTER_TYPE__ pointer_t;
 
 /* rounds up to the nearest multiple of ALIGNMENT */
 #define ALIGN(size) (((size) + (DSIZE - 1)) & ~0x7)
 #define PACK(size, alloc) ((size) | (alloc))
 #define SIZE_T_SIZE (ALIGN(sizeof(size_t)))
 
-#define GET(p) (*(unsigned int *)(p))
-#define PUT(p, val) (*(unsigned int *)(p) = (val))
+#define GET(p) (*(pointer_t *)(p))
+#define PUT(p, val) (*(pointer_t *)(p) = (val))
+#define PUT_ADD(p, add) (*(pointer_t *)(p) = (pointer_t *)(add))
 
 #define GET_SIZE(p) (GET(p) & ~0x7)
 #define GET_ALLOC(p) (GET(p) & 0x1)
@@ -66,14 +69,15 @@ team_t team = {
 
 static void *find_linked(void *ptr)
 {
-    char *now_element = (char *)mem_heap_lo() + DSIZE;
-    while (now_element < (char *)mem_heap_hi())
+    pointer_t *now_element = (pointer_t *)(mem_heap_lo() + DSIZE);
+    while (1)
     {
-        if (now_element > (char *)ptr)
+        pointer_t *now_suc = GOTO_SUCC(now_element);
+        if (now_element > (pointer_t *)ptr || !GOTO_SUCC(now_element) || GOTO_SUCC(now_element) == ptr)
         {
             return now_element;
         }
-        now_element = GOTO_SUCC(ptr);
+        now_element = (pointer_t *)GOTO_SUCC(now_element);
     }
     return NULL; // 끝까지 갔는데 없으면 이 블록이 링크드리스트의 끝에 붙는거
 }
@@ -86,18 +90,23 @@ static void *coalesce(void *ptr)
     // list_prev = PRED(list_next)
 
     size_t prev_allocated = GET_ALLOC(GET_PREV_FTR(ptr));
+    pointer_t *should_be_epilogue = NEXT_BLKP(ptr); // 3번 프리하고나서 합칠 때 에필로그가 0이어야 하는데 아님
     size_t next_allocated = GET_ALLOC(GET_NEXT_HDR(ptr));
     size_t now_size = GET_SIZE(HDRP(ptr));
-    void *list_next = find_linked(ptr);
-    void *list_prev = GOTO_PRED(list_next);
+    pointer_t *list_prev = find_linked(ptr);
+    pointer_t *list_next = GOTO_SUCC(list_prev) ? GOTO_PRED(list_prev) : NULL;
 
     if (prev_allocated && next_allocated)
     {
         // 합칠게 없으면 SUCC(list_prev) = ptr, PRED(ptr) = list_prev , PREV(next) = ptr, SUCC(ptr) = list_next
-        PUT(SUCC(list_prev), ptr);
-        PUT(PRED(ptr), list_prev);
-        PUT(PRED(list_next), ptr);
-        PUT(SUCC(ptr), list_next);
+        PUT_ADD(SUCC(list_prev), ptr);
+        PUT_ADD(PRED(ptr), list_prev);
+        if (list_next)
+        {
+            PUT_ADD(PRED(list_next), ptr);
+        }
+
+        PUT_ADD(SUCC(ptr), list_next);
     }
     else if (prev_allocated && !next_allocated)
     {
@@ -105,11 +114,15 @@ static void *coalesce(void *ptr)
         now_size += GET_SIZE(GET_NEXT_HDR(ptr));
         PUT(HDRP(ptr), PACK(now_size, 0));
         PUT(FTRP(ptr), PACK(now_size, 0));
-        PUT(SUCC(list_prev), ptr);
-        PUT(PRED(ptr), list_prev);
-        PUT(SUCC(ptr), SUCC(list_next));
-        PUT(SUCC(list_next), ptr);
+        PUT_ADD(SUCC(list_prev), ptr);
+        PUT_ADD(PRED(ptr), list_prev);
+        PUT_ADD(SUCC(ptr), SUCC(list_next));
+        if (list_next)
+        { //?
+            PUT_ADD(SUCC(list_next), ptr);
+        }
     }
+
     else if (!prev_allocated && next_allocated)
     {
         // 이전꺼랑 합치면 링크드리스트는 업데이트 없음.
@@ -124,8 +137,11 @@ static void *coalesce(void *ptr)
         now_size += GET_SIZE(GET_PREV_FTR(ptr)) + GET_SIZE(GET_NEXT_HDR(ptr));
         PUT(GET_PREV_HDR(ptr), PACK(now_size, 0));
         PUT(GET_NEXT_FTR(ptr), PACK(now_size, 0));
-        PUT(SUCC(list_prev), BLKP_BY_SUCC(SUCC(list_next)));
-        PUT(PREV(SUCC(list_next)), list_prev);
+        PUT_ADD(SUCC(list_prev), BLKP_BY_SUCC(SUCC(list_next)));
+        if (list_next)
+        {
+            PUT_ADD(PRED(SUCC(list_next)), list_prev);
+        }
         ptr = PREV_BLKP(ptr);
     }
     return ptr;
@@ -144,7 +160,7 @@ static void *extend_heap(size_t words)
     PUT(HDRP(start_p), PACK(size, 0));
     PUT(FTRP(start_p), PACK(size, 0));
     PUT(GET_NEXT_HDR(start_p), PACK(0, 1)); // 마지막 워드에 에필로그 넣어주기
-
+    pointer_t *epilogue = GET_NEXT_HDR(start_p);
     return coalesce(start_p);
 }
 
@@ -153,18 +169,17 @@ static void *extend_heap(size_t words)
  */
 int mm_init(void)
 {
-    char *heap_startp = mem_sbrk(4 * WSIZE);
+    char *heap_startp = mem_sbrk(6 * WSIZE);
     if (heap_startp == (void *)-1)
     {
         return -1;
     }
-    PUT(heap_startp, 0);                              // 패딩
-    PUT(heap_startp + WSIZE, PACK(2 * DSIZE, 1));     // 프롤로그 헤더
-    PUT(heap_startp + WSIZE * 2, NULL);               // 힙의 첫번째 predecessor = root 역할
-    PUT(heap_startp + WSIZE * 3, NULL);               // successor
-    PUT(heap_startp + WSIZE * 4, PACK(2 * DSIZE, 1)); // 프롤로그 푸터
-    PUT(heap_startp + WSIZE * 5, PACK(0, 1));         // 에필로그
-
+    PUT(heap_startp, 0);                                         // 패딩
+    PUT(heap_startp + WSIZE, PACK(2 * DSIZE, 1));                // 프롤로그 헤더
+    PUT(heap_startp + WSIZE * 2, NULL);                          // 힙의 첫번째 predecessor = root 역할
+    PUT_ADD(heap_startp + WSIZE * 3, (heap_startp + 6 * WSIZE)); // successor
+    PUT(heap_startp + WSIZE * 4, PACK(2 * DSIZE, 1));            // 프롤로그 푸터
+    PUT(heap_startp + WSIZE * 5, PACK(0, 1));                    // 에필로그
     if (extend_heap(CHUNKSIZE / WSIZE) == NULL)
     {
         return -1;
@@ -174,7 +189,7 @@ int mm_init(void)
 
 static void *find_fit(size_t asize)
 {
-    char *find_p = (char *)mem_heap_lo() + DSIZE; // root (힙의 첫번째 pred)
+    pointer_t *find_p = (pointer_t *)(mem_heap_lo() + DSIZE); // root (힙의 첫번째 pred)
 
     // 할당할 수 있다면 포인터 리턴.
     // 할당할 수 없으면 현재 포인터를 *successor로 업데이트 해서 윗줄로 돌아가기.
@@ -182,7 +197,7 @@ static void *find_fit(size_t asize)
 
     while (1)
     {
-        if (find_p == NULL)
+        if (!find_p)
         {
             return NULL;
         }
@@ -190,7 +205,7 @@ static void *find_fit(size_t asize)
         {
             return (void *)find_p;
         }
-        find_p = GOTO_SUCC(find_p);
+        find_p = (pointer_t *)GOTO_SUCC(find_p);
     }
 }
 
@@ -205,18 +220,19 @@ static void place(void *bp, size_t asize)
         PUT(GET_NEXT_HDR(bp), PACK(free_size - asize, 0));
         PUT(GET_NEXT_FTR(bp), PACK(free_size - asize, 0));
         // 쪼개진 뒷 블럭 링크드리스트 그 자리에 그대로 넣어주기
-        PUT(SUCC(PRED(bp)), NEXT_BLKP(bp));
-        PUT(PRED(NEXT_BLKP(bp)), PRED(bp));
-        PUT(PRED(SUCC(bp)), NEXT_BLKP(bp));
-        PUT(SUCC(NEXT_BLKP(bp)), BLKP_BY_SUCC(SUCC(bp)));
+        // 링크드리스트에서 블록 빼고 작아진 주소값 그대로 그 자리에
+        PUT_ADD(SUCC(PRED(bp)), NEXT_BLKP(bp)); // 루트의 succ에 현재 블록 넣어주기
+        PUT_ADD(PRED(NEXT_BLKP(bp)), PRED(bp));
+        PUT_ADD(PRED(SUCC(bp)), NEXT_BLKP(bp));               // 여기 고쳐주기(SUCC(bp)=NULL)일때
+        PUT_ADD(SUCC(NEXT_BLKP(bp)), BLKP_BY_SUCC(SUCC(bp))); // 근데 널이 안들어간 것 같음
     }
     else
     {
         PUT(HDRP(bp), PACK(free_size, 1));
         PUT(FTRP(bp), PACK(free_size, 1));
         // 링크드리스트에서 할당하는 블록 빼주기
-        PUT(SUCC(PRED(bp)), BLKP_BY_SUCC(SUCC(bp)));
-        PUT(PRED(SUCC(bp)), PRED(bp));
+        PUT_ADD(SUCC(PRED(bp)), BLKP_BY_SUCC(SUCC(bp)));
+        PUT_ADD(PRED(SUCC(bp)), PRED(bp));
     }
 }
 
@@ -227,6 +243,7 @@ static void place(void *bp, size_t asize)
 
 void *mm_malloc(size_t size)
 {
+    printf("malloc called");
     size_t asize;
     char *bp;
 
@@ -259,6 +276,7 @@ void *mm_malloc(size_t size)
  */
 void mm_free(void *ptr)
 {
+    printf("free called");
     size_t size = GET_SIZE(HDRP(ptr));
     PUT(HDRP(ptr), PACK(size, 0));
     PUT(FTRP(ptr), PACK(size, 0));
